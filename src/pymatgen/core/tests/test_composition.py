naik-aakash/pymatgen-core

commit a4ad965c35989a7a68f3b2709ae604be07083c49

Add Cs2O2 and Rb2O2 to the list of special formulas.
Former-commit-id: 4e74c7e90e7ac228bef36ce5014acfcb469e1b95 [formerly e1def1cf637b4b52e891163bf8391eb920f95807]
Former-commit-id: 507c15b3a18e95a4fb4c9fd17bcc9b2f95fd9819

src/pymatgen/core/composition.py
@@ -76,8 +76,8 @@ class Composition(collections.Mapping, collections.Hashable, MSONable):
     that formula output does not write LiO instead of Li2O2 for example.
     """
     special_formulas = {"LiO": "Li2O2", "NaO": "Na2O2", "KO": "K2O2",
-                        "HO": "H2O2", "O": "O2", "F": "F2", "N": "N2",
-                        "Cl": "Cl2", "H": "H2"}
+                        "HO": "H2O2", "CsO": "Cs2O2", "RbO": "Rb2O2",
+                        "O": "O2",  "N": "N2", "Cl": "Cl2", "H": "H2"}
 
     def __init__(self, *args, **kwargs):
         """
@@ -741,8 +741,7 @@ def reduce_formula(sym_amt):
     syms = sorted(sym_amt.keys(),
                   key=lambda s: smart_element_or_specie(s).X)
 
-    syms = filter(lambda s: sym_amt[s] > Composition.amount_tolerance,
-                  syms)
+    syms = filter(lambda s: sym_amt[s] > Composition.amount_tolerance, syms)
     num_el = len(syms)
     contains_polyanion = (num_el >= 3 and
                           smart_element_or_specie(syms[num_el - 1]).X

commit 2bacdf8520e0c61c99c7b7d67916e1c53b0dae55

Re-add "F":"F2" to special formulas dictionary looks like a regression in commit a4ad965c35989a7a68f3b2709ae604be07083c49 [formerly 507c15b3a18e95a4fb4c9fd17bcc9b2f95fd9819] [formerly 4e74c7e90e7ac228bef36ce5014acfcb469e1b95 [formerly e1def1cf637b4b52e891163bf8391eb920f95807]] Fixes #172, though I still worry about the fragility of this code
Former-commit-id: 41341f17f5b921ae918f9bf47df3dfb4539bfe45 [formerly 8ff25c373b20f09346939a2c2677c6a1849fd564]
Former-commit-id: c267ea65dd08648f2f39075f406ab33a74a624fa

src/pymatgen/core/composition.py
@@ -85,7 +85,8 @@ class Composition(collections.Mapping, collections.Hashable, PMGSONable):
     """
     special_formulas = {"LiO": "Li2O2", "NaO": "Na2O2", "KO": "K2O2",
                         "HO": "H2O2", "CsO": "Cs2O2", "RbO": "Rb2O2",
-                        "O": "O2",  "N": "N2", "Cl": "Cl2", "H": "H2"}
+                        "O": "O2",  "N": "N2", "F": "F2", "Cl": "Cl2",
+                        "H": "H2"}
 
     def __init__(self, *args, **kwargs): #allow_negative=False
         """

src/pymatgen/core/tests/test_composition.py
@@ -316,6 +316,14 @@ class CompositionTest(unittest.TestCase):
         self.assertEqual(c1.element_composition, Composition())
         self.assertEqual(c1.average_electroneg, 1.31)
 
+    def test_special_formulas(self):
+        special_formulas = {"LiO": "Li2O2", "NaO": "Na2O2", "KO": "K2O2",
+                            "HO": "H2O2", "CsO": "Cs2O2", "RbO": "Rb2O2",
+                            "O": "O2",  "N": "N2", "F": "F2", "Cl": "Cl2",
+                            "H": "H2"}
+        for k, v in special_formulas.items():
+            self.assertEqual(Composition(k).reduced_formula, v)
+
 
 class ChemicalPotentialTest(unittest.TestCase):
 